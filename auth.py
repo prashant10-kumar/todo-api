f387ef45488a04feb6f4e189384dba9d9f151f19
prashant10-kumar <prashantk4644@gmail.com>
2026-06-03 00:09:59 +0530

    "Adding the login and register features"

--- auth.py
+++ auth.py
@@ -1,20 +1,26 @@
 from jose import JWTError, jwt
-from passlib.context import CryptContext
 from datetime import datetime, timedelta
 import os
+import bcrypt
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
-pwd_context = CryptContext(schemes = ["bcrypt"], deprecated = "auto")
 
-def hash_password(password: str):
-    return pwd_context.hash(password)
+
+def hash_password(password: str)->str:
+    return bcrypt.hashpw(
+        password.encode("utf-8"),
+        bcrypt.gensalt()
+    ).decode("utf-8")
 
 def verify_password(plain_password: str, hashed_password:str):
-    return pwd_context.vereify(plain_password, hashed_password)
+    return bcrypt.checkpw(
+        plain_password.encode("utf-8"),
+        hashed_password.encode("utf-8")
+    )
 
-def create_access_token(data: dict):
+def create_access_token(data: dict)->str:
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes = ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp" : expire})
--- main1.py
+++ main1.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer , OAuth2PasswordRequestForm
 from pydantic import BaseModel, Field
 from typing import List
 from sqlalchemy.orm import Session
 import models
+import auth
 from database import SessionLocal, engine
+
+class UserCreate(BaseModel):
+    username : str = Field(min_length = 3)
+    password : str = Field(min_length = 6)
+
+class Token(BaseModel):
+    access_token : str
+    token_type : str
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl = "login")
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -43,6 +55,30 @@ def add_todo(todo: TodoCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_todo)
     return new_todo 
+
+@app.post("/register")
+def register(user: UserCreate, db: Session = Depends(get_db)):
+    existing_user = db.query(models.User).filter(
+        models.User.username == user.username
+    ).first()
+    if existing_user:
+        raise HTTPException(status_code = 400, detail = "Username already exists")
+    hashed = auth.hash_password(user.password)
+    new_user = models.User(username = user.username, password = hashed)
+    db.add(new_user)
+    db.commit()
+    return {"message" : "User registerd successfully"}
+
+@app.post("/login", response_model = Token)
+def login(from_data: OAuth2PasswordRequestForm = Depends(),
+          db : Session = Depends(get_db)):
+    user = db.query(models.User).filter(
+        models.User.username == from_data.username
+    ).first()
+    if not user or not auth.verify_password(from_data.password, user.password):
+        raise HTTPException(status_code = 401, details = "Invalid credentials")
+    token = auth.create_access_token(data = {"sub": user.username})
+    return {"access_token": token, "token_type": "bearer"}
 
 @app.delete("/todos/{id}")
 def del_todo(id: int, db: Session = Depends(get_db)):
